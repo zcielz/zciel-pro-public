ca752c796f8ed20f5fadfcb2ecf963973fa629ef
zciel <zciel_plus@outlook.com>
2019-02-28 00:20:44 +0800

    algorithm commit

--- algorithm/src/main/java/cn/zciel/niukejichu/basic_class_03/Code_01_Array_To_Stack_Queue.java
+++ algorithm/src/main/java/cn/zciel/niukejichu/basic_class_03/Code_01_Array_To_Stack_Queue.java
@@ -1,5 +1,7 @@
 package cn.zciel.niukejichu.basic_class_03;
 
+import java.lang.reflect.Array;
+
 /**
  * @author YINZHEN
  * @date 2019/2/27 18:28
@@ -47,9 +49,49 @@ public class Code_01_Array_To_Stack_Queue { //实现栈
 
     public static class ArrayQueue {  //实现队列
         private Integer[] arr;
-        private Integer size;
-        private Integer first;
-        private Integer last;
+        private Integer size;  //约束first 和 last 的行为
+        private Integer first; //开始位置
+        private Integer last;  //结束位置
+
+        private ArrayQueue(int initSize) {
+            if (initSize < 0) {
+                throw new IllegalArgumentException("the init size is less then 0, error");
+            }
+            arr = new Integer[initSize];
+            size = 0;
+            first = 0;
+            last = 0;
+        }
+
+        public Integer peek() {
+            if (size == 0) {
+                return null;
+            }
+            return arr[first];
+        }
+
+        public void push(int obj) {
+            if (size == arr.length) {
+                throw new ArrayIndexOutOfBoundsException("the queue is full");
+            }
+            size++;
+            arr[last] = obj;
+            last = last == arr.length - 1 ? 0 : last + 1; //如果end到达了最后一个位置调回到0，如果没有就++
+        }
+
+        public Integer poll() {
+            if (size == 0) {
+                throw new ArrayIndexOutOfBoundsException("the queue is empty");
+            }
+            size--;
+            int tmp = first;
+            first = first == arr.length - 1 ? 0 : first + 1; //同上返回
+            return arr[tmp];
+        }
+
+        public static void main(String[] args) {
+
+        }
 
 
     }
---
+++ algorithm/src/main/java/cn/zciel/niukejichu/basic_class_03/Code_02_GetMinStack.java
@@ -0,0 +1,104 @@
+package cn.zciel.niukejichu.basic_class_03;
+
+import java.util.Stack;
+
+public class Code_02_GetMinStack {
+
+    public static class MyStack1 {
+        private Stack<Integer> stackData;
+        private Stack<Integer> stackMin;
+
+        public MyStack1() {
+            this.stackData = new Stack<Integer>();
+            this.stackMin = new Stack<Integer>();
+        }
+
+        public void push(int newNum) {
+            if (this.stackMin.isEmpty()) {
+                this.stackMin.push(newNum);
+            } else if (newNum <= this.getmin()) {
+                this.stackMin.push(newNum);
+            }
+            this.stackData.push(newNum);
+        }
+
+        public int pop() {
+            if (this.stackData.isEmpty()) {
+                throw new RuntimeException("Your stack is empty.");
+            }
+            int value = this.stackData.pop();
+            if (value == this.getmin()) {
+                this.stackMin.pop();
+            }
+            return value;
+        }
+
+        public int getmin() {
+            if (this.stackMin.isEmpty()) {
+                throw new RuntimeException("Your stack is empty.");
+            }
+            return this.stackMin.peek();
+        }
+    }
+
+    public static class MyStack2 {
+        private Stack<Integer> stackData;
+        private Stack<Integer> stackMin;
+
+        public MyStack2() {
+            this.stackData = new Stack<Integer>();
+            this.stackMin = new Stack<Integer>();
+        }
+
+        public void push(int newNum) {
+            if (this.stackMin.isEmpty()) { //如果为空直接传入一个
+                this.stackMin.push(newNum);
+            } else if (newNum <= this.getmin()) {
+                this.stackMin.push(newNum); //新的比较小，就直接传入
+            } else {
+                int newMin = this.stackMin.peek(); //如果不大，就重复压入值
+                this.stackMin.push(newMin);
+            }
+            this.stackData.push(newNum); //原来的也要传进来
+        }
+
+        public int pop() {
+            if (this.stackData.isEmpty()) {
+                throw new RuntimeException("You stack is Empty");
+            }
+            this.stackMin.pop();
+            return this.stackData.pop();//返回的是原来数组的pop
+        }
+
+        private int getmin() {
+            if (this.stackMin.isEmpty()) {
+                throw new RuntimeException("You stack is empty.");
+            }
+            return this.stackMin.peek();
+        }
+    }
+
+    public static void main(String[] args) {
+        MyStack1 stack1 = new MyStack1();
+        stack1.push(3);
+        System.out.println(stack1.getmin());
+        stack1.push(4);
+        System.out.println(stack1.getmin());
+        stack1.push(1);
+        System.out.println(stack1.getmin());
+        System.out.println(stack1.pop());
+        System.out.println(stack1.getmin());
+
+        System.out.println("=============");
+
+        MyStack1 stack2 = new MyStack1();
+        stack2.push(3);
+        System.out.println(stack2.getmin());
+        stack2.push(4);
+        System.out.println(stack2.getmin());
+        stack2.push(1);
+        System.out.println(stack2.getmin());
+        System.out.println(stack2.pop());
+        System.out.println(stack2.getmin());
+    }
+}
